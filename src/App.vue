1873ed6efb73caa8dd5a907362e346149d45d5c3
darianaren <darianarengifo@gmail.com>
2026-04-14 10:16:13 -0300

    [FEAT] Create Budget component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup></script>
+<script setup>
+import Budget from "./components/Budget.vue";
+</script>
 
 <template>
   <div>
     <header>
       <h1>Planificador de Gastos</h1>
+      <div class="container-header container shadow">
+        <Budget />
+      </div>
     </header>
   </div>
 </template>
@@ -11,6 +16,7 @@
 <style>
 :root {
   --blue: #3b82f6;
+  --blue-dark: #1758e5;
   --white: #fff;
   --gray-light: #f5f5f5;
   --gray: #94a3b8;
@@ -19,28 +25,44 @@
 }
 
 html {
-  font-size: 62.5%;
   box-sizing: border-box;
 }
 
 *,
 *::before,
 *::after {
+  font-size: 0.9rem;
   box-sizing: inherit;
+
+  @media (min-width: 768px) {
+    font-size: 1rem;
+    box-sizing: inherit;
+  }
 }
 
 body {
-  font-size: 1.6rem;
   font-family: "Lato", sans-serif;
   background-color: var(--gray-light);
 }
 
 h1 {
-  font-size: 4rem;
+  font-size: 2rem;
+  @media (min-width: 768px) {
+    font-size: 2.5rem;
+  }
+  @media (min-width: 1300px) {
+    font-size: 4rem;
+  }
 }
 
 h2 {
-  font-size: 3rem;
+  font-size: 1rem;
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+  }
+  @media (min-width: 1300px) {
+    font-size: 3rem;
+  }
 }
 
 header {
@@ -51,6 +73,42 @@ header {
     margin: 0;
     color: var(--white);
     text-align: center;
+    line-height: 1;
+  }
+}
+
+.container {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.container-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 2rem;
+  @media (min-width: 768px) {
+    padding: 4rem;
+    padding-top: 3rem;
+  }
+  @media (min-width: 1300px) {
+    padding: 5rem;
+    padding-top: 4rem;
+  }
+}
+
+.shadow {
+  box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.1);
+  background-color: var(--white);
+  border-radius: 1.2rem;
+  padding: 2rem;
+  @media (min-width: 768px) {
+    padding: 4rem;
+    padding-top: 3rem;
+  }
+  @media (min-width: 1300px) {
+    padding: 5rem;
+    padding-top: 4rem;
   }
 }
 </style>
---
+++ src/components/Budget.vue
@@ -0,0 +1,60 @@
+<script></script>
+
+<template>
+  <form class="budget-form">
+    <div class="field">
+      <label class="label" for="new-budget">Definir Presupuesto</label>
+      <input
+        class="input"
+        type="number"
+        id="new-budget"
+        placeholder="Ingresa tu presupuesto"
+      />
+    </div>
+    <input class="submit" type="submit" value="Añadir Presupuesto" />
+  </form>
+</template>
+
+<style scoped>
+.budget-form {
+  width: 100%;
+}
+.field {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+.label {
+  font-weight: 700;
+  font-size: 1.2rem;
+  text-align: center;
+  color: var(--blue);
+}
+.input {
+  width: 100%;
+  padding: 1rem;
+  border: none;
+  border-radius: 1rem;
+  background-color: var(--gray-light);
+  text-align: center;
+}
+.submit {
+  color: var(--white);
+  font-weight: 900;
+  text-align: center;
+
+  margin-top: 1.5rem;
+  padding: 1rem;
+
+  width: 100%;
+
+  border: none;
+  border-radius: 1rem;
+  background-color: var(--blue);
+  transition: background-color 0.3s ease;
+}
+.submit:hover {
+  background-color: var(--blue-dark);
+  cursor: pointer;
+}
+</style>
