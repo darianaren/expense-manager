17ec04a4d3b77f3a6711dc33bc173edafc531b74
darianaren <darianarengifo@gmail.com>
2026-04-14 09:26:33 -0300

    [CHORE] Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <HelloWorld />
+  <div>
+    <header>
+      <h1>Planificador de Gastos</h1>
+    </header>
+  </div>
 </template>
+
+<style>
+:root {
+  --blue: #3b82f6;
+  --white: #fff;
+  --gray-light: #f5f5f5;
+  --gray: #94a3b8;
+  --gray-dark: #64748b;
+  --black: #000;
+}
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gray-light);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+header {
+  background-color: var(--blue);
+
+  h1 {
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--white);
+    text-align: center;
+  }
+}
+</style>
